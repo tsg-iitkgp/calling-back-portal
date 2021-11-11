c2ef64f95574babc7c666259e7f594f58d3c0859
Praneeth Kolanu <k.praneeth1199@iitkgp.ac.in>
2021-11-11 22:05:02 +0530

    Dashboard template added and routes configured

--- main/urls.py
+++ main/urls.py
@@ -14,6 +14,11 @@ app_name = 'main'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('auth/login', views.login, name='login'),
+    path('auth/logout', views.logout, name='logout'),
+    path('dashboard', views.dashboard, name='dashboard'),
+    path('dashboard/pg', views.pg_dashboard, name='pg_dashboard'),
+    path('dashboard/rs', views.rs_dashboard, name='rs_dashboard'),
     # path('get_quote/', views.get_quote, name='get_quote'),
     # path('<int:year>/', views.diary),
     # path('<int:year>/<str:name>/', views.diary),
--- main/views.py
+++ main/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import logout as logout_fn
 
 # Create your views here.
 def index(request):
-    return HttpResponse(('Hello World'))
+    return redirect('/dashboard')
+
+def login(request):
+    return render(request, 'main/login.html')
+
+def logout(request):
+    logout_fn(request)
+    return redirect('/auth/login')
+
+def dashboard(request):
+    return render(request, 'main/index.html')
+
+def pg_dashboard(request):
+    return render(request, 'main/index.html')
+
+def rs_dashboard(request):
+    return render(request, 'main/index.html')
+
+def test_api(request):
+    print()
+    return HttpResponse('API Tested')
